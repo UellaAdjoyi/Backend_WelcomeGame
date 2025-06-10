cca37029d46d904419b0b032c600c8f658ab076d
haozhihan1 <zhihanhao1@gmail.com>
2025-06-10 12:02:44 +0200

    cros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->addend(\Fruitcake\Cors\HandleCors::class);//
+        $middleware->prepend(\Fruitcake\Cors\HandleCors::class);//
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
